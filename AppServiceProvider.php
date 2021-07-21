339ceb6de855dd66022fc12324ef8d861b12eb3d
Miguel Fernando Marlés Betancourt <80717402+ferchitoM@users.noreply.github.com>
2021-07-20 21:47:20 -0500

    Subir proyecto al servidor
    Creando las variables de entorno como estan en el archivo .env y modificando los archivos provider y proxies funciona el servidor y la autenticación por email.

--- AppServiceProvider.php
+++ AppServiceProvider.php
@@ -6,49 +6,57 @@ use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-
         VerifyEmail::toMailUsing(function ($user, string $verificationUrl) {
             return (new MailMessage)
-                ->from('nocontestar_sera@sena.edu.co', 'Soporte Tecnico SERAP')
+                ->from('nocontestar_recorrido@gmail.com', 'Soporte técnico RECORRIDO')
                 ->greeting(Lang::get('Hola!'))
-                ->subject(Lang::get('Verificacion De correo electronico'))
-                ->line(Lang::get('Por favor, haga click en el boton de abajo para verificar su direccion de correo electronico'))
-                ->action(Lang::get('Verificar correo electronico'), $verificationUrl)
-                ->line(Lang::get('Si no creo una cuenta, no es necesario realizar ninguna otra accion.'))
-                ->salutation(new HtmlString("Saludos,<br>SERAP"));
+                ->subject(Lang::get('Verificación de correo electrónico'))
+                ->line(Lang::get('Por favor, haga click en el botón de abajo para verificar su direccion de correo electronico'))
+                ->action(Lang::get('Verificar correo electrónico'), $verificationUrl)
+                ->line(Lang::get('Si no creó una cuenta, no es necesario realizar ninguna otra acción.'))
+                ->salutation(new HtmlString("Saludos,<br>RECORRIDO"));
         });
 
         ResetPassword::toMailUsing(function ($user, string $token) {
-
             return (new MailMessage)
-                ->from('nocontestar_sera@sena.edu.co', 'Soporte Tecnico SERAP')
+                ->from('nocontestar_recorrido@gmail.com', 'Soporte técnico RECORRIDO')
                 ->greeting(Lang::get('Hola!'))
-                ->subject(Lang::get('Notificacion de restablecimiento de contraseña'))
-                ->line(Lang::get('Has recibido este correo electronico porque tenemos una solicitud de restablecimiento de contraseña'))
-                ->action(Lang::get('Restablecer contraseña'), url(route('password.reset',
-                        [
-                            'token' => $token,
-                            'email' => $user->getEmailForPasswordReset(),
-                        ],
-                        false)))
-                ->line(Lang::get('El enlace para restablecer la contraseña caducara en 60 minuntos.'))
-                ->salutation(new HtmlString("Saludos,<br>SERAP"));
+                ->subject(Lang::get('Notificación de restablecimiento de contraseña'))
+                ->line(Lang::get('Recibió este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para su cuenta.'))
+                ->action(Lang::get('Reestablecer contraseña'), url(route(
+                    'password.reset',
+                    [
+                        'token' => $token,
+                        'email' => $user->getEmailForPasswordReset(),
+                    ],
+                    false
+                )))
+                ->line(Lang::get('Este enlace para restablecer la contraseña caducará en 60 minutos.'))
+                ->salutation(new HtmlString("Saludos,<br>RECORRIDO VIRTUAL SENA"));
         });
     }
 
-
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
-        // Force SSL in production        
+        // Force SSL in production
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
